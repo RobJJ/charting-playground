24f74e3cc97f72ded432f08b9dfb484e5eb0a446
RobJ <robjjohnsenwork@gmail.com>
2023-03-29 11:18:05 +0700

    changing props on click,, still need animate

--- src/charts/highcharts/scatter-bubble-chart-v2.js
+++ src/charts/highcharts/scatter-bubble-chart-v2.js
@@ -6,54 +6,56 @@ import HighchartsReact from "highcharts-react-official";
 import HC_more from "highcharts/highcharts-more";
 HC_more(Highcharts);
 //
+const getOriginalData = () => [
+  {
+    x: 0,
+    y: 0,
+    z: 100, // set the size of the bubble
+  },
+];
+const getAlternativeData = () => [
+  {
+    x: 10,
+    y: 10,
+    z: 20, // set the size of the first country bubble
+  },
+  {
+    x: 20,
+    y: 20,
+    z: 30, // set the size of the second country bubble
+  },
+  {
+    x: 30,
+    y: 30,
+    z: 50, // set the size of the third country bubble
+  },
+];
+//
+const options = {
+  chart: {
+    type: "bubble",
+    animation: {
+      duration: 2000, // set the duration of the animation in milliseconds
+    },
+  },
+  series: [
+    {
+      name: "World",
+      data: getOriginalData(),
+    },
+  ],
+};
+//
 const BubbleChartVersion2 = () => {
   const chartRef = useRef(null);
   const [chart, setChart] = useState(null);
-  const getOriginalData = () => [
-    {
-      x: 0,
-      y: 0,
-      z: 100, // set the size of the bubble
-    },
-  ];
-  const options = {
-    chart: {
-      type: "bubble",
-      animation: {
-        duration: 2000, // set the duration of the animation in milliseconds
-      },
-    },
-    series: [
-      {
-        name: "World",
-        data: getOriginalData(),
-      },
-    ],
-  };
 
   useEffect(() => {
     setChart(Highcharts.chart(chartRef.current, options));
   }, []);
 
-  const getAlternativeData = () => [
-    {
-      x: 10,
-      y: 10,
-      z: 20, // set the size of the first country bubble
-    },
-    {
-      x: 20,
-      y: 20,
-      z: 30, // set the size of the second country bubble
-    },
-    {
-      x: 30,
-      y: 30,
-      z: 50, // set the size of the third country bubble
-    },
-  ];
-
   const handleClick = () => {
+    chart.series[0].update({ name: "Countries" });
     chart.series[0].setData(getAlternativeData());
   };
 
