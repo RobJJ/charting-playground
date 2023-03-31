1913cb1c5a232cd882ea3583c5aeb94c4141dfb6
RobJ <robjjohnsenwork@gmail.com>
2023-03-31 11:23:44 +0700

    testing diff events and animation triggers

--- src/charts/highcharts/scatter-bubble-chart-v2.js
+++ src/charts/highcharts/scatter-bubble-chart-v2.js
@@ -68,8 +68,8 @@ const BubbleChartVersion2 = () => {
     chart: {
       type: "bubble",
       events: {
-        redraw: function (event) {
-          console.log(event);
+        load: function (e) {
+          console.log(this.options);
           var label = this.renderer
             .label("A series was added, about to redraw chart", 100, 120)
             .attr({
