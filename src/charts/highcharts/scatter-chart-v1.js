c208f05b576a59e27646788c2cf1c1a04ca14696
RobJ <robjjohnsenwork@gmail.com>
2023-04-14 10:11:45 +0700

    added test data for scatter - building test layout

--- src/charts/highcharts/scatter-chart-v1.js
+++ src/charts/highcharts/scatter-chart-v1.js
@@ -3,8 +3,13 @@ import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import highchartsMore from "highcharts/highcharts-more";
 
+import { scatterData } from "./data-scatter-v1";
+
+highchartsMore(Highcharts);
+
 const ScatterChart = () => {
   let chart;
+  const [data, setData] = useState(scatterData);
   const [chartOptions, setChartOptions] = useState({
     chart: {
       type: "scatter",
@@ -22,6 +27,45 @@ const ScatterChart = () => {
       // text: _.startCase(`Experimal build: Highchart: ${type} chart`),
       text: `Experimental build - HighCharts - Scatter chart`,
     },
+    xAxis: {
+      title: {
+        text: "Environment Score",
+      },
+      gridLineWidth: 1,
+      //   min: 2000,
+      //   max: 100000,
+      startOnTick: false,
+      endOnTick: true,
+      // maxPadding: 0.2,
+      tickLength: 0,
+    },
+    yAxis: {
+      title: {
+        text: "Economic Score",
+      },
+      //   min: 50,
+      //   max: 100,
+      startOnTick: false,
+      endOnTick: false,
+      maxPadding: 0.2,
+    },
+    series: [
+      {
+        // start with points that dont have parent
+        data: data,
+        states: {
+          hover: {
+            enabled: false,
+          },
+        },
+        dataLabels: {
+          enabled: true,
+          format: "{point.district}",
+        },
+
+        cursor: "pointer",
+      },
+    ],
   });
 
   return (
